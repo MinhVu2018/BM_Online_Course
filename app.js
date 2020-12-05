eaafe792bac7e0c28c4ac89efd83f22ee98e8538
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-12-05 12:23:46 +0700

    Add footer to page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var ejs = require('ejs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
