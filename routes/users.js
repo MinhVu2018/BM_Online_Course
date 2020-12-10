1faa804cd9065bc5941c9f7ee526611ffe780656
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-12-10 13:19:02 +0700

    User regis account to database

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,52 @@ var express = require('express');
 var router = express.Router();
 const db = require('../utils/db');
 const bcrypt = require('bcrypt');
+const { add } = require('../utils/db');
 
 /* GET users listing. */
 router.get('/regis', function(req, res, next) {
-  res.render('account/signup', { title: 'Regisation' });
+  res.render('account/signup');
 });
 
-router.post('/regis', function(req, res, next) {
+router.post('/regis', async function(req, res, next) {
   var user = {
     "userName": req.body.name,
     "password": req.body.psw,
     "email": req.body.email,
-    "type": "User"
+    "type": "user"
   }
 
   const rePass = req.body.re_psw;
-  console.log(user)
-  if (user.password.localeCompare(rePass) != 0) {
-    res.send("Your password is not the same!");
+
+  if (user.userName.length > 10) {
+    res.render('account/noti', {
+      status: "Oppps",
+      message: "Your user name is too long"
+    })
+    return;
+  }
+  console.log(user, rePass)
+  if (!(user.password === rePass)) {
+    res.render('account/noti', {
+      status: "Oppps",
+      message: "Your repeat password is not correct"
+    })
+    return;
   }
 
-  db.add(user, 'Users')
+  user.password = bcrypt.hashSync(user.password, 10);
+  db.add(user, 'Users');
+  res.render('account/noti', {
+    status: "Congratulation",
+    message: "Your account has been regis success."
+  })
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('account/login');
+});
+
+router.post('/login', function(rq, res, next) {
+
+});
 module.exports = router;
